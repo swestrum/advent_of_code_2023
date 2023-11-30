2b7e1c60ada955508868410ec83a1b70ffa321e8
Stephanie Westrum <greensirus@gmail.com>
2023-11-30 17:34:25 -0600

    Revert "added at least a skeleton for day 1 and basic info into README"
    This reverts commit 21ad807e3dedfd435ae1daed779418243fd64e5d.

--- day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/Users/stephanie/Documents/advent_of_code_2023/day1/target/debug/day1: /Users/stephanie/Documents/advent_of_code_2023/day1/src/main.rs
--- day1/target/debug/deps/day1-90deea1fa1d1b181.d
+++
@@ -1,5 +0,0 @@
-/Users/stephanie/Documents/advent_of_code_2023/day1/target/debug/deps/day1-90deea1fa1d1b181: src/main.rs
-
-/Users/stephanie/Documents/advent_of_code_2023/day1/target/debug/deps/day1-90deea1fa1d1b181.d: src/main.rs
-
-src/main.rs:

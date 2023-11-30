21ad807e3dedfd435ae1daed779418243fd64e5d
Stephanie Westrum <greensirus@gmail.com>
2023-11-30 17:03:27 -0600

    added at least a skeleton for day 1 and basic info into README

---
+++ day1/target/debug/day1.d
@@ -0,0 +1,1 @@
+/Users/stephanie/Documents/advent_of_code_2023/day1/target/debug/day1: /Users/stephanie/Documents/advent_of_code_2023/day1/src/main.rs
---
+++ day1/target/debug/deps/day1-90deea1fa1d1b181.d
@@ -0,0 +1,5 @@
+/Users/stephanie/Documents/advent_of_code_2023/day1/target/debug/deps/day1-90deea1fa1d1b181: src/main.rs
+
+/Users/stephanie/Documents/advent_of_code_2023/day1/target/debug/deps/day1-90deea1fa1d1b181.d: src/main.rs
+
+src/main.rs:

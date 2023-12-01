d99388eee68126da5f9a4fa9c03ec7425844341f
Stephanie Westrum <greensirus@gmail.com>
2023-11-30 18:20:40 -0600

    added skeleton for running part 1 and part 2 separately

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,9 +1,56 @@
-// use std::env;
+use std::env;
+use colored::*;
+
+fn part_one(){
+    println!("{}\n", "PART ONE".blue())
+}
+
+fn part_two(){
+    println!("{}\n", "PART TWO".blue())
+}
+
+fn help(){
+    println!("{}\n", "Please pass in either 1 or 2".yellow().bold())
+}
 
 fn main() {
-    // let args: Vec<String> = env::args().collect();
-    // dbg!(args);
+    let args: Vec<String> = env::args().collect();
     let day: usize = 1;
     let christmas_emoji: String = "\u{1F384}".to_string();
-    println!("\nDAY {} {}\n",  day, christmas_emoji.repeat(day));
+    match args.len() {
+        1 => {
+            println!("\n{} {} {}\n", "DAY".green(), day.to_string().green(), christmas_emoji.repeat(day));
+            part_one();
+            part_two();
+        }
+        2 => {
+            println!("\n{} {} {}\n", "DAY".green(), day.to_string().green(), christmas_emoji.repeat(day));
+            let num = &args[1];
+            let number: i32 = match num.parse() {
+                Ok(n) => {
+                    n
+                },
+                Err(_) => {
+                    eprintln!("error: second argument not an integer");
+                    help();
+                    return;
+                },
+            };
+            match number {
+                1 => {
+                    part_one();
+                }
+                2 => {
+                    part_two();
+                }
+                _ => {
+                    eprintln!("error: second argument not a valid part");
+                    help();
+                }
+            }
+        }
+        _ => {
+            help();
+        }
+    }
 }
